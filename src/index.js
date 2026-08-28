42e6c7726cb08f2dfd78b23675a0db5850a59469
Deepali-Jaiswal <deepalijaiswal1295@gmail.com>
2026-08-28 21:03:12 +0530

    api errors , db connections , success massage

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+    .then(() => {
+        app.on("error", (error)=>{
+            console.log("error", error);
+            throw error
+        })
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port , ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("MONGO db connection failed", error)
+    })
+
+connectDB(); 
